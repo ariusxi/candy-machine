af324479f911def50274c75d876686900936431e
Alef Felix <aleffelix@MacBook-Air.Dlink>
2021-09-09 20:37:44 -0300

    Base machine

---
+++ src/main/CandyMachine.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Machine from './../components/Machine'
+
+import './CandyMachine.css'
+
+class CandyMachine extends Component {
+
+    render() {
+        return (
+            <Machine>
+                Candy Machine
+            </Machine>
+        )
+    }
+
+}
+
+export default CandyMachine

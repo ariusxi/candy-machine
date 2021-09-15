b8f4cb53ea1c1951b84bc106e280a82643ca3850
Bruno Leiva <bruno.leivapires2706@gmail.com>
2021-09-14 21:01:05 -0300

    Retirando botão de vend e adicionando campo do valor do doce

--- src/components/Vend/index.jsx
+++ src/components/Vend/index.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 
 import './styles.css'
 
+
 const Vend = _ => {
 	return (
 		<div className="Vend">
 			<form>
 				<input 
-					type="text"
-					name="selection"
+					type="number"
+					name="candyValue"
 					required="true"
-					placeholder="xx"
+					placeholder="0.00"
 					maxlength="2"
-					pattern="[a-zA-Z]{1}[0-9]{1}"/>
+					pattern="d+(.d{2})?"/>
 				<input
 					type="number"
 					name="coinage"
 					required="true"
 					placeholder="0.00"
 					step="0.10"
-					value="0.00"
+					value="9.00"
 					pattern="d+(.d{2})?"/>
-				<button type="submit">Vend</button>
+
 			</form>
 		</div>
 	)

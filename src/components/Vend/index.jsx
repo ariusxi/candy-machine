b2d65cfffe9083f6c131ee681bc8dee8c67d71b3
Bruno Leiva <bruno.leivapires2706@gmail.com>
2021-09-14 23:39:58 -0300

    Selecionando doce pelo numero e passando o valor ficticio para o componente de VEND

--- src/components/Vend/index.jsx
+++ src/components/Vend/index.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 import './styles.css'
 
-const Vend = _ => {
+
+const Vend = props => {
+	const { candyValue } = props
+
 	return (
 		<div className="Vend">
 			<form>
-			<input 
+				<input
 					type="number"
 					name="candyValue"
 					required="true"
 					placeholder="0.00"
+					value={candyValue}
 					maxlength="2"
-					pattern="d+(.d{2})?"/>
+					pattern="d+(.d{2})?" />
 				<input
 					type="number"
 					name="coinage"
@@ -20,7 +24,7 @@ const Vend = _ => {
 					placeholder="0.00"
 					step="0.10"
 					value="0.00"
-					pattern="d+(.d{2})?"/>
+					pattern="d+(.d{2})?" />
 
 			</form>
 		</div>

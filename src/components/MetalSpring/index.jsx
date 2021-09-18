8c6a537d79f13b978aa003a5ef712daefeabb50b
Alef Felix <aleffelix@MacBook-Air.Dlink>
2021-09-18 17:10:42 -0300

    Rollback metal spring

--- src/components/MetalSpring/index.jsx
+++ src/components/MetalSpring/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import Spring from './../../assets/images/spiral.svg'
+import { ReactComponent as Spring } from './../../assets/images/spiral.svg'
 
 import './styles.css'
 
 const MetalSpring = _ => {
 	return (
-		<img src={Spring} alt={Spring}/>
+		<Spring
+			fill="grey"
+			className="MetalSpring"/>
 	)
 }
 

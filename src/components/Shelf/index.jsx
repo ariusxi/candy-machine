89a555fcdc37ddc46b68d51edd559bd1ef5d73d7
Alef Felix <aleffelix@MacBook-Air.Dlink>
2021-09-20 19:44:21 -0300

    Adding some new colors to the machine

--- src/components/Shelf/index.jsx
+++ src/components/Shelf/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import Candy from '../Candy'
 
-import MetalSpring from './../MetalSpring'
-
 import './styles.css'
 
 const Shelf = props => {
 	return (
 		<div className="Shelf">
 			<div className="item">
-				<MetalSpring/>
 				<Candy
 					candyImage={props.candyImage}
 					isFullSize={props.isFullSize} />
